5a76edc5330c9655354570bdb38433f372069312
Ann Wolff <annwolffe@gmail.com>
2016-12-08 07:38:12 -0700

    Adds index to trips db to shorten searches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207224559) do
+ActiveRecord::Schema.define(version: 20161208143026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20161207224559) do
     t.datetime "updated_at",       null: false
     t.date     "start_date"
     t.date     "end_date"
+    t.index ["start_date"], name: "index_trips_on_start_date", using: :btree
   end
 
 end
